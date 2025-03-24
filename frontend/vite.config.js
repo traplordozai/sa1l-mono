a668553094832fe2c38ae61c2b01042200a14979
traplordozai <38846662+traplordozai@users.noreply.github.com>
2025-03-24 02:27:05 -0400

    update package.json and vite.config.js: change start script to use vite preview and update import syntax

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,7 @@
+import { defineConfig } from 'vite';
 const { defineConfig } = require('vite');
 const react = require('@vitejs/plugin-react');
+
 
 module.exports = defineConfig({
   plugins: [react()],
