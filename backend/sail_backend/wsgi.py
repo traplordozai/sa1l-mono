0563dc755006a5ac52d78c8412869ff4d53c1b41
traplordozai <38846662+traplordozai@users.noreply.github.com>
2025-03-24 02:52:21 -0400

    enhance Dockerfile and wsgi.py: add python-dotenv for environment variable management

--- backend/sail_backend/wsgi.py
+++ backend/sail_backend/wsgi.py
@@ -1,4 +1,6 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sail_backend.settings")
